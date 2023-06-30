0a94a4c31619d3f74bd30a0cd783992a3c9ecb92
QuocKhanh392001 <98048557+QuocKhanh392001@users.noreply.github.com>
2023-06-30 17:05:56 +0700

    Update thuattoan.js
    Optimal code

--- thuattoan.js
+++ thuattoan.js
@@ -1,27 +1,20 @@
-function findMax() {
-    let max = -Infinity;
-    for (let i = 0; i < arguments.length; i++) {
-      if (arguments[i] > max) {
-        max = arguments[i];
-      }
-    }
-    return max;
-  }
-  function findMin() {
-    let min = Infinity;
-    for (let i = 0; i < arguments.length; i++) {
-      if (arguments[i] < min) {
-        min = arguments[i];
-      }
-    }
-    return min;
-  }
-  function miniMaxSum(a,b,c,d,e) {
+function miniMaxSum(a,b,c,d,e) {
     let suma = b + c + d + e;
     let sumb = a + c + d + e;
     let sumc = a + b + d + e;
     let sumd = a + b + c + e;
     let sume = a + b + c + d;
-    let result = findMax(suma, sumb, sumc, sumd, sume) + " " + findMin(suma, sumb, sumc, sumd, sume);
+    let arr = [suma, sumb, sumc, sumd, sume];
+    let max = suma;
+    let min = suma;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+      else if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+    let result = min + " " + max;
     return result ;
   }
